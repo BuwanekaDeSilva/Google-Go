cde814f8764b94f67b5d00625128b5e3bd7554d6
bpdesilva <buwanekapasan@gmail.com>
2019-05-14 01:23:26 +0530

    html templates

--- Web/Templates/main.go
+++ Web/Templates/main.go
@@ -6,11 +6,21 @@ import (
 	"net/http"
 )
 
+type Page struct {
+	Name string
+}
+
 func main() {
+
 	templates := template.Must(template.ParseFiles("templates/index.html"))
+	p := Page{Name: "Gopher"}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if name := r.FormValue("name"); name != "" {
+			p.Name = name
+		}
 		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
-			http.Error(w, err.Error(), http.InternalServerError)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
 
