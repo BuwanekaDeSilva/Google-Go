acb8fff73a4e082cbcbd13fdda22cd827cc24a1e
bpdesilva <buwanekapasan@gmail.com>
2019-05-14 11:20:37 +0530

    sqlite3 lib

--- Web/Connect-a-DB/main.go
+++ Web/Connect-a-DB/main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
 )
 
 type Page struct {
-	Name string
+	Name     string
+	DBStatus bool
 }
 
 func main() {
 
 	templates := template.Must(template.ParseFiles("templates/index.html"))
 
+	db, _ := sql.Open("sqlite3", "dev.db")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		p := Page{Name: "Gopher"}
 		if name := r.FormValue("name"); name != "" {
 			p.Name = name
 		}
+		p.DBStatus = db.Ping() == nil
 		if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
